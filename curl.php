44edfb8673fe5a8974b518dc332533c1fca447e2
Harish <harish@localhost.localdomain>
2015-03-05 21:46:44 +0530

    fifth commit

--- curl.php
+++ curl.php
@@ -1,34 +1,50 @@
 <?php
+/*
+	Anyone is free to copy, modify, publish, use, compile, sell, or
+	distribute this software, either in source code form or as a compiled
+	binary, for any purpose, commercial or non-commercial, and by any
+	means.
+*/
 
-// example of how to use basic selector to retrieve HTML contents
-include('./simplehtmldom_1_5/simple_html_dom.php');
-    	$curl = curl_init();
-    	curl_setopt($curl, CURLOPT_URL, "http://cs75.tv/2012/summer/");
-    	curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($curl, CURLOPT_FOLLOWLOCATION, TRUE);
-	curl_setopt($curl, CURLOPT_AUTOREFERER, TRUE);
-	$result = curl_exec ($curl);
-	curl_close ($curl);
+/*      
+	Includes a library written at simplehtmldom.sourceforge.net for parsing the 
+	HTML.Use of a regular expression is also preferred but can sometimes result
+	in bad results.
+*/
+	include('./simplehtmldom_1_5/simple_html_dom.php');
 
-// get DOM from URL or file
+// Create a DOM from URL or file
 	$html = file_get_html('http://cs75.tv/2012/summer/');
 
 	$num = 0;
-// find all link
+// searches for all links in the parsed $html
 	foreach($html->find('a') as $e) {
 		$links = $e->href." ";
+/*
+	Specify any term that was found in the required URL in strpos and like in this case
+	the .mp4 extension of the file to be downloaded. Make sure the site doesn't have
+	two separate links for viewing and for download, and if it does have, then
+	download-link's extension has to be provided like here '.mp4.download' .
+ 
+*/
 		if (strpos($links,'.mp4.download') !== false) {
 			$num++;
 			echo $links;
+// created to set indefinte time to download. 	
 			set_time_limit(0);
-			$fp = fopen ('/home/harish/'.$num.'.mp4', 'w+');
-		$ch = curl_init($links);
-		curl_setopt($ch, CURLOPT_TIMEOUT, 50);
-		curl_setopt($ch, CURLOPT_FILE, $fp);
-		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-		curl_exec($ch); // get curl response
-		curl_close($ch);
-		fclose($fp);
+/*	
+	specify the directory here as the first argument for fopen or the download will be in your 
+	current working directory which probably would be 'curl-links-master' where you extracted .
+	
+*/
+			$fp = fopen ($num.'.mp4', 'w+');
+			$ch = curl_init($links);
+			curl_setopt($ch, CURLOPT_TIMEOUT, 50);
+			curl_setopt($ch, CURLOPT_FILE, $fp);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+			curl_exec($ch); 
+			curl_close($ch);
+			fclose($fp);
+		}
 	}
-}
 ?>
